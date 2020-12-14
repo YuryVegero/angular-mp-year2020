679a7324eb8ba4c5bcba777838445c632ec60814
Yury Viahera <yuravegero@gmail.com>
2020-12-14 07:20:43 +0300

    09-Observables: onPush for LoadingIndicator

--- src/app/shared/components/loading-indicator/loading-indicator.component.ts
+++ src/app/shared/components/loading-indicator/loading-indicator.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
 @Component({
   selector: 'mp-loading-indicator',
   templateUrl: './loading-indicator.component.html',
-  styleUrls: [ './loading-indicator.component.scss' ]
+  styleUrls: [ './loading-indicator.component.scss' ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LoadingIndicatorComponent {
   @Input() text = 'Loading ...';
